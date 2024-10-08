df86785f8ce1a040f18609dcd716476cb3c69764
Jorge Cuarón <kerloom@yahoo.com>
2024-10-08 14:27:45 -0600

    Solution with minHeap

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,37 @@
 "use strict";
 
-// Print all entries, across all of the *async* sources, in chronological order.
+const MinHeap = require("../lib/min-heap");
 
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+  return new Promise(async (resolve, reject) => {
+    try {
+      const heap = new MinHeap();
+
+      await Promise.all(
+        logSources.map(async (source, index) => {
+          const entry = await source.popAsync();
+          if (entry) {
+            heap.insert({ sourceIndex: index, entry });
+          }
+        })
+      );
+
+      // Process and print log entries in chronological order
+      while (heap.size() > 0) {
+        const { sourceIndex, entry } = heap.extractMin();
+
+        printer.print(entry);
+
+        // Get the next log entry from the same source
+        const nextEntry = await logSources[sourceIndex].popAsync();
+        if (nextEntry) {
+          heap.insert({ sourceIndex, entry: nextEntry });
+        }
+      }
+      printer.done();
+      resolve();
+    } catch (error) {
+      reject(error);
+    }
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,27 @@
 "use strict";
 
-// Print all entries, across all of the sources, in chronological order.
+const MinHeap = require("../lib/min-heap");
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const heap = new MinHeap();
+
+  for (let i = 0; i < logSources.length; i++) {
+    const entry = logSources[i].pop();
+    if (entry) {
+      heap.insert({ sourceIndex: i, entry });
+    }
+  }
+
+  while (heap.size() > 0) {
+    const { sourceIndex, entry } = heap.extractMin();
+
+    printer.print(entry);
+
+    const nextEntry = logSources[sourceIndex].pop();
+    if (nextEntry) {
+      heap.insert({ sourceIndex, entry: nextEntry });
+    }
+  }
+
+  printer.done();
 };
