51205b3c5880c3098a4fb2a3f58649cb12db14fe
Jorge Cuarón <kerloom@yahoo.com>
2024-10-21 12:17:44 -0600

    Add a buffer to async solution, fix shared memory in popAsync

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,41 +2,48 @@
 
 const MinHeap = require("../lib/min-heap");
 
+const BUFFER_SIZE = 15;
+
+// Helper function to fill the buffer for a given source
+async function fillBuffer(source, buffer) {
+  while (buffer.length < BUFFER_SIZE && !source.drained) {
+    const entryPromises = [];
+    for (let i = buffer.length; i < BUFFER_SIZE; i++) {
+      entryPromises.push(source.popAsync());
+    }
+    const entries = await Promise.all(entryPromises);
+    buffer.push(...entries.filter((entry) => entry != false));
+  }
+}
+
 module.exports = (logSources, printer) => {
   return new Promise(async (resolve, reject) => {
     try {
-      /* For the async solution, I tried speeding up with batches, but if we are memory bound, it does not help much.
-      If for example, we have 100 sources and a batch of 10, then we would need 1000 entries in memory to process the next 10 logs,
-      and on each iteration we are adding 1000 entries.
-      */
       const heap = new MinHeap();
+      const buffers = logSources.map(() => []);
 
-      const promises = logSources.map(async (source, index) => {
-        return source
-          .popAsync()
-          .then((entry) => ({ sourceIndex: index, entry }));
-      });
-
-      const results = await Promise.all(promises);
-
-      results.forEach(({ sourceIndex, entry }) => {
-        if (entry) {
-          heap.insert({ sourceIndex, entry });
-        }
-      });
-
-      // Process and print log entries in chronological order
-      while (heap.size() > 0) {
-        const { sourceIndex, entry } = heap.extractMin();
-
-        printer.print(entry);
-
-        // Get the next log entry from the same source
-        const nextEntry = await logSources[sourceIndex].popAsync();
-        if (nextEntry) {
-          heap.insert({ sourceIndex, entry: nextEntry });
+      // Initialize the heap with the first entry from each source
+      for (let i = 0; i < logSources.length; i++) {
+        await fillBuffer(logSources[i], buffers[i]);
+        if (buffers[i].length > 0) {
+          heap.insert({ sourceIndex: i, entry: buffers[i][0] });
         }
       }
+
+      while (heap.size() > 0) {
+        const { sourceIndex, entry } = heap.extractMin();
+        printer.print(entry);
+
+        // Delete the printed entry from the buffer and refill if it's empty
+        buffers[sourceIndex].shift();
+        if (buffers[sourceIndex].length === 0) {
+          await fillBuffer(logSources[sourceIndex], buffers[sourceIndex]);
+        }
+        if (buffers[sourceIndex].length > 0) {
+          heap.insert({ sourceIndex, entry: buffers[sourceIndex][0] });
+        }
+      }
+
       printer.done();
       resolve();
     } catch (error) {
